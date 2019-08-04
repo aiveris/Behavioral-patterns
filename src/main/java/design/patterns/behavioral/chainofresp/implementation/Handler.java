e757f42d071f0641ce632a8a7ff6ec8c15e3622d
aiveris <staind0323>
2019-08-04 09:24:33 +0300

    2019-08-03

--- src/main/java/design/patterns/behavioral/chainofresp/implementation/Handler.java
+++ src/main/java/design/patterns/behavioral/chainofresp/implementation/Handler.java
@@ -3,12 +3,13 @@ package design.patterns.behavioral.chainofresp.implementation;
 public abstract class Handler {
 
 	protected Handler successor;
-	
+
 	public Handler setSuccessor(Handler successor) {
 		this.successor = successor;
+		// ... if you want a flent and hierachical interface, like such: director.setSuccessor(vicePresident).setSuccessor(ceo);
+		// ... then be careful not to return "this" here as it would just reset the success for the director
 		return successor;
 	}
-	
+
 	public abstract void handleRequest(Request request);
-	
 }
